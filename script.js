013f4422a6e9eb5dd4a3cc2a22433c64d18faa3b
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-09-10 21:35:00 -0700

    code Updated

--- script.js
+++ script.js
@@ -10,16 +10,51 @@ skillButton.addEventListener("click", function () {
     }
     else {
         skillHideShow.innerHTML = `<div><h3>Skills</h3></div>
-            <ul class="skills-list">
-              <li>Typescript</li>
-              <li>Next js</li>
-              <li>React</li>
-              <li>HTML/CSS</li>
-              <li>Adobe Illustrator</li>
-              <li>Adobe Photoshop</li>
-              <li>Adobe AfterEffect</li>
-            </ul>`;
+    <ul class="skills-list">
+    <li>Typescript</li>
+    <li>Next js</li>
+    <li>React</li>
+    <li>HTML/CSS</li>
+    <li>Adobe Illustrator</li>
+    <li>Adobe Photoshop</li>
+    <li>Adobe AfterEffect</li>
+    </ul>`;
         skillButton.innerText = "Hide Skills";
         flag = 0;
     }
+});
+/* Color Changing Setting*/
+/* All Button */
+let blueBtn = document.querySelector(".blue");
+let redBtn = document.querySelector(".red");
+let greenBtn = document.querySelector(".green");
+let orangeBtn = document.querySelector(".orange");
+/* Colors Variables*/
+const blueColor = "rgb(0, 88, 252)";
+/* All Elements Need to be changed */
+let shapeOne = document.querySelector(".shape1");
+let shapeTwo = document.querySelector(".shape2");
+let imageBorder = document.querySelector("img");
+let Head1 = document.querySelector("h1");
+let iconPhone = document.querySelector(".fa-phone");
+let iconEmail = document.querySelector(".fa-envelope");
+let iconAddress = document.querySelector(".fa-location-dot");
+let InfoEdu = document.querySelector(".infoEducation");
+let InfoExp = document.querySelector(".infoExperience");
+let InfoSki = document.querySelector(".infoSkills");
+let copyRight = document.querySelector(".copyRights");
+blueBtn.addEventListener("click", function () {
+    shapeOne.style.backgroundColor = blueColor;
+    shapeOne.style.color = blueColor;
+    shapeTwo.style.color = blueColor;
+    shapeTwo.style.backgroundColor = blueColor;
+    imageBorder.style.borderColor = blueColor;
+    Head1.style.color = blueColor;
+    iconPhone.style.color = blueColor;
+    iconEmail.style.color = blueColor;
+    iconAddress.style.color = blueColor;
+    InfoEdu.style.backgroundColor = blueColor;
+    InfoExp.style.backgroundColor = blueColor;
+    InfoSki.style.backgroundColor = blueColor;
+    copyRight.style.borderColor = blueColor;
 });
--- script.ts
+++ script.ts
@@ -10,16 +10,64 @@ skillButton.addEventListener("click", function () {
     flag = 1;
   } else {
     skillHideShow.innerHTML = `<div><h3>Skills</h3></div>
-            <ul class="skills-list">
-              <li>Typescript</li>
-              <li>Next js</li>
-              <li>React</li>
-              <li>HTML/CSS</li>
-              <li>Adobe Illustrator</li>
-              <li>Adobe Photoshop</li>
-              <li>Adobe AfterEffect</li>
-            </ul>`;
+    <ul class="skills-list">
+    <li>Typescript</li>
+    <li>Next js</li>
+    <li>React</li>
+    <li>HTML/CSS</li>
+    <li>Adobe Illustrator</li>
+    <li>Adobe Photoshop</li>
+    <li>Adobe AfterEffect</li>
+    </ul>`;
     skillButton.innerText = "Hide Skills";
     flag = 0;
   }
+});
+
+/* Color Changing Setting*/
+/* All Button */
+let blueBtn = document.querySelector(".blue") as HTMLButtonElement;
+let redBtn = document.querySelector(".red") as HTMLButtonElement;
+let greenBtn = document.querySelector(".green") as HTMLButtonElement;
+let orangeBtn = document.querySelector(".orange") as HTMLButtonElement;
+/* Colors Variables*/
+const blueColor: string = "rgb(0, 88, 252)";
+/* All Elements Need to be changed */
+let shapeOne = document.querySelector(".shape1") as HTMLDivElement;
+let shapeTwo = document.querySelector(".shape2") as HTMLDivElement;
+
+let imageBorder = document.querySelector("img") as HTMLImageElement;
+
+let Head1 = document.querySelector("h1") as HTMLElement;
+
+let iconPhone = document.querySelector(".fa-phone") as HTMLElement;
+let iconEmail = document.querySelector(".fa-envelope") as HTMLElement;
+let iconAddress = document.querySelector(".fa-location-dot") as HTMLElement;
+
+let InfoEdu = document.querySelector(".infoEducation") as HTMLDivElement;
+let InfoExp = document.querySelector(".infoExperience") as HTMLDivElement;
+let InfoSki = document.querySelector(".infoSkills") as HTMLDivElement;
+
+let copyRight = document.querySelector(".copyRights") as HTMLDivElement;
+
+blueBtn.addEventListener("click", function () {
+  shapeOne.style.backgroundColor = blueColor;
+  shapeOne.style.color = blueColor;
+
+  shapeTwo.style.color = blueColor;
+  shapeTwo.style.backgroundColor = blueColor;
+
+  imageBorder.style.borderColor = blueColor;
+
+  Head1.style.color = blueColor;
+
+  iconPhone.style.color = blueColor;
+  iconEmail.style.color = blueColor;
+  iconAddress.style.color = blueColor;
+
+  InfoEdu.style.backgroundColor = blueColor;
+  InfoExp.style.backgroundColor = blueColor;
+  InfoSki.style.backgroundColor = blueColor;
+
+  copyRight.style.borderColor = blueColor;
 });
